6c1b3bd6de131fe16d52cdfeceb025ee2725b7ea
Samuel Tselnik <139260316+stselnik@users.noreply.github.com>
2024-08-18 22:13:23 -0400

    style: Simple info added to navbar

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -2,8 +2,9 @@
 
 export default function Navbar() {
     return(
-        <div className="flex relative bg-white text-black p-2">
+        <div className="flex items-end gap-2 relative bg-white text-black p-2">
             <h1 className="text-2xl">Wolfie Ride Bike Availability</h1>
+            <h2 className="font-light">Check what stations at Stony Brook have bikes near you.</h2>
         </div>
 
     )
