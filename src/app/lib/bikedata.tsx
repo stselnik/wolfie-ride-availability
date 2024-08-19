4e3be6d5bdc6f10ea54a270f4c32c1879981c383
Samuel Tselnik <139260316+stselnik@users.noreply.github.com>
2024-08-18 22:10:22 -0400

    fix: Changed API endpoint to HTTPS - Intended to fix "insecure content" error on deployment.

--- src/app/lib/bikedata.tsx
+++ src/app/lib/bikedata.tsx
@@ -2,7 +2,7 @@
 
 
 export default async function GetStationsData() {
-    let endpoint = "http://api.citybik.es/v2/networks/wolf-ride-bike-share";
+    let endpoint = "https://api.citybik.es/v2/networks/wolf-ride-bike-share";
     const request = await fetch(endpoint);
     const response = await request.json();
     //console.log(response.network.stations);
